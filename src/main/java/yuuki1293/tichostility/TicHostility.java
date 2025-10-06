32d623dfa61f1ab2269df96346d5d8c2a88f95c3
yuuki1293 <yuuki1293@outlook.jp>
2025-10-06 17:17:52 +0900

    chore: remove unused

--- src/main/java/yuuki1293/tichostility/TicHostility.java
+++ src/main/java/yuuki1293/tichostility/TicHostility.java
@@ -5,7 +5,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -15,12 +14,10 @@ import org.slf4j.Logger;
 
 @Mod(TicHostility.MODID)
 public class TicHostility {
-    public static final String MODID = "tic-hostility";
+    public static final String MODID = "tichostility";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public TicHostility(FMLJavaModLoadingContext context) {
-        IEventBus modEventBus = context.getModEventBus();
-
         MinecraftForge.EVENT_BUS.register(this);
 
         context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
